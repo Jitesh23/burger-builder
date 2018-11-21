07de26c21cda6de957a4caab965581dc78adf4ad
Jitesh23 <jiteshpowankar@gmail.com>
2018-11-21 19:36:36 +0530

    Details:
    - Added the removing ingredient functionality
    - Showing the current price
    - Added the order now button

--- src/components/Burger/BuildControlles/BuildControlles.js
+++ src/components/Burger/BuildControlles/BuildControlles.js
@@ -13,14 +13,21 @@ const controls = [
 
 const buildControlles = (props) => (
     <div className={classes.BuildControlles}> 
+        <p><strong>Current Price: {props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
             <BuildControl 
             key = {ctrl.label} 
             label = {ctrl.label} 
             added = {() => props.ingredientAdded(ctrl.type)} 
+            removed = {() => props.ingredientRemoved(ctrl.type)}
+            disabled = {props.disabled[ctrl.type]}
 
             />
         ))}
+        <button 
+        className= {classes.OrderButton}
+        disabled= {!props.purchasable}
+      >ORDER NOW</button>
     </div>
 )
 
